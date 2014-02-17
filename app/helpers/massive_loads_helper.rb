ebb058919eed0fdc15587f831877aa4bbd3e43b5
Jorge Luna <seguidor777@gmail.com>
2014-02-17 15:28:41 -0700

    Se hicieron modificaciones en la carga masiva.

--- app/helpers/massive_loads_helper.rb
+++ app/helpers/massive_loads_helper.rb
@@ -15,11 +15,10 @@ module MassiveLoadsHelper
 
     2.upto(s.last_row) do |line|
       begin
-#        unless @event.attendees.blank?
-#          inc_id = @event.attendees.last.attendee_id.gsub(@event.token_for_id, "").to_i + 1
-#        end
+        unless @event.attendees.blank?
+          inc_id = @event.attendees.last.attendee_id.gsub(@event.token_for_id, "").to_i + 1
+        end
         attendee_id = @event.token_for_id + "%04d" % inc_id#s.cell(line, "A")
-        inc_id += 1
         subgroup_key = "S1"#s.cell(line, "B")
         #e_name = s.cell(line, "C")
         e_tradename = s.cell(line, "A")#s.cell(line, "D")
@@ -94,7 +93,7 @@ module MassiveLoadsHelper
         a_job = "N/A" if a_job.nil?
         subgroup_id = @event.subgroups.find_by_subgroup_key(subgroup_key).id
         if attendee_id[0, 2] == @event.token_for_id
-          @attendee = Attendee.new(subgroup_id: subgroup_id, e_name: e_name, e_tradename: e_tradename, e_street: e_street, e_ext_number: e_ext_number, e_int_number: e_int_number, e_colony: e_colony, e_municipality: e_municipality, e_city: e_city, e_state: e_state, e_zip_code: e_zip_code, e_rfc: e_rfc, e_lada: e_lada, e_phone: e_phone, a_name: a_name, a_email: a_email, a_chat: a_chat, a_cellphone: a_cellphone, a_tel_nextel: a_tel_nextel, a_radio_nextel: a_radio_nextel, a_is_director: a_is_director, a_platform: a_platform, e_main_line: e_main_line, a_sec_line: a_sec_line, a_num_employees: a_num_employees, a_other_line: a_other_line, a_web: a_web, a_market_segment: a_market_segment, attendee_id: attendee_id, event_id: session[:current_event_id], confirmation_token: Array.new(10) {[*'0'..'9', *'a'..'z'].sample}.join, a_sector: a_sector, a_want_email: a_want_email, a_job: a_job)
+          @attendee = @event.attendees.new(subgroup_id: subgroup_id, e_name: e_name, e_tradename: e_tradename, e_street: e_street, e_ext_number: e_ext_number, e_int_number: e_int_number, e_colony: e_colony, e_municipality: e_municipality, e_city: e_city, e_state: e_state, e_zip_code: e_zip_code, e_rfc: e_rfc, e_lada: e_lada, e_phone: e_phone, a_name: a_name, a_email: a_email, a_chat: a_chat, a_cellphone: a_cellphone, a_tel_nextel: a_tel_nextel, a_radio_nextel: a_radio_nextel, a_is_director: a_is_director, a_platform: a_platform, e_main_line: e_main_line, a_sec_line: a_sec_line, a_num_employees: a_num_employees, a_other_line: a_other_line, a_web: a_web, a_market_segment: a_market_segment, attendee_id: attendee_id, confirmation_token: Array.new(10) {[*'0'..'9', *'a'..'z'].sample}.join, a_sector: a_sector, a_want_email: a_want_email, a_job: a_job)
           if @attendee.save
             #AttendeeMailer.welcome_email(@attendee).deliver!
           else
