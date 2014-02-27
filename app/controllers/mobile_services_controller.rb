eb92b91a7a29667a5b9ef455b8590acf61c42e45
Jorge Luna <seguidor777@gmail.com>
2014-02-27 15:39:33 -0700

    Se modificó el controlador de los servicios web.

--- app/controllers/mobile_services_controller.rb
+++ app/controllers/mobile_services_controller.rb
@@ -58,7 +58,7 @@ class MobileServicesController < ApplicationController
             end
       
           else
-            @msg = { name: nil, email: @email, subgroup_name: @subgroup_name, group_name: @group_name, subgroup_leader: @subgroup_leader, domain: @domain, enterprise: @enterprise, phone: @phone, address: @address, msg: t("errors.atten_email_already_sended"), sent: "no" }
+            @msg = { name: nil, email: @email, subgroup_name: @subgroup_name, group_name: @group_name, subgroup_leader: @subgroup_leader, domain: @domain, enterprise: @enterprise, phone: @phone, address: @address, msg: t("errors.atten_email_already_sended"), sent: "ok" }
           end
         
 #        else
