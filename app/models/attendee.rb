707e3ba8159b9bc16983044cb74d4b63531e9001
Jorge Luna <seguidor777@gmail.com>
2014-10-02 14:54:11 -0600

    Cambios a carga masiva.

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -10,7 +10,8 @@ class Attendee < ActiveRecord::Base
   has_many :attendee_expositions, :dependent => :destroy
   has_many :attendee_workshops, :dependent => :destroy
   
-  validates :a_email, :a_name, :e_tradename, :e_name, :event_id, :confirmation_token, :a_sector, :a_job, :presence => true
+  validates :a_name, :e_tradename, :e_name, :event_id, :presence => true
+  #validates :a_email, :confirmation_token, :a_sector, :a_job, :presence => true
   validates_numericality_of :a_num_employees, :if => :a_num_employees
   validates_numericality_of :e_ext_number, :if => :e_ext_number
   validates_numericality_of :e_lada, :if => :e_lada
